010dbbc4bb4e3a3d9b5b495d208d316cb3753466
HannaZahorodnia <zahorodnia.h@gmail.com>
2018-11-29 20:07:13 +0100

    Выполняет подключение к MySQL, отправляет SQL-запросы для получения списка лотов и категорий

--- templates/index.php
+++ templates/index.php
@@ -3,15 +3,11 @@
     <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и
         горнолыжное снаряжение.</p>
     <ul class="promo__list">
-        <?php
-        $index = 0;
-        $num = count($categories);
-        while ($index < $num): ?>
+        <?php foreach ($categories as $category): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?=$categories[$index];?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?=$category['name'];?></a>
             </li>
-            <?php $index++; ?>
-        <?php endwhile; ?>
+        <?php endforeach; ?>
     </ul>
 </section>
 <section class="lots">
@@ -19,18 +15,18 @@
         <h2>Открытые лоты</h2>
     </div>
     <ul class="lots__list">
-        <?php foreach ($lots as $key => $val): ?>
+        <?php foreach ($lots as $key => $lot): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$val['img']; ?>" width="350" height="260" alt="">
+                    <img src="<?=$lot['photo_path']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$val['category'];?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($val['name']);?></a></h3>
+                    <span class="lot__category"><?=$lot['category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($lot['title']);?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=htmlspecialchars(formatPrice($val['price']));?></span>
+                            <span class="lot__cost"><?=htmlspecialchars(formatPrice($lot['start_price']));?></span>
                         </div>
                         <div class="lot__timer timer">
                             <?=showTimeLeft();?>
