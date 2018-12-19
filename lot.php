55223622f348dd567bc523e5d9b935f009ab9d96
HannaZahorodnia <zahorodnia.h@gmail.com>
2018-12-19 18:35:14 +0100

    fixup! Добавляет вывод ошибки при невыбранной категории на add.php

--- lot.php
+++ lot.php
@@ -13,14 +13,16 @@ if (empty($_SESSION['user'])) {
     $user_avatar = $_SESSION['user']['photo_path'] ? $_SESSION['user']['photo_path'] : "";
 }
 
+$lot_id = intval($_GET['id']);
 
-if (!isset($_GET['id']) || !getAvailableLot($_GET['id'], $con)) {
+
+if (!isset($lot_id) || !getAvailableLot($lot_id, $con)) {
     header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
 
 } else if ($con) {
-    $sql_lot_by_id = showLotById(intval($_GET['id']));
+    $sql_lot_by_id = showLotById($lot_id);
     $current_lot = getData($con, $sql_lot_by_id);
-    $current_lot_price = getData($con, showMaxBid(intval($_GET['id'])));
+    $current_lot_price = getData($con, showMaxBid($lot_id));
     $current_lot[0]['price'] = $current_lot_price[0]['price'];
 
 
@@ -35,13 +37,17 @@ if (!isset($_GET['id']) || !getAvailableLot($_GET['id'], $con)) {
         }
     }
 
-    $sql_bids_num = showBidsNum(intval($_GET['id']));
+    $sql_bids_num = showBidsNum($lot_id);
     $bids_num = getData($con, $sql_bids_num);
 
-    $sql_bid_by_id = showBidById(intval($_GET['id']));
+    $sql_bid_by_id = showBidById($lot_id);
     $current_bid = getData($con, $sql_bid_by_id);
 
-    if (isset($_SESSION['user']['id']) && $_SESSION['user']['id'] === $_SESSION['current_lot']['author']) {
+
+    $_SESSION['current_lot'] = $current_lot[0];
+    $lot_author = $_SESSION['current_lot']['author'];
+
+    if (isset($_SESSION['user']['id']) && $_SESSION['user']['id'] === $lot_author) {
         $author_id = true;
     } else {
         $author_id = false;
@@ -49,7 +55,6 @@ if (!isset($_GET['id']) || !getAvailableLot($_GET['id'], $con)) {
 
     $page_content = include_template('lot-index.php', ['lot' => $current_lot[0], 'history' => $bids_num[0], 'bid' => $current_bid, 'author_id' => $author_id]);
     $layout_content = include_template('lot-layout.php', ['user_name' => $user_name, 'user_avatar' => $user_avatar, 'content' => $page_content, 'title' => $current_lot[0]['title']]);
-    $_SESSION['current_lot'] = $current_lot[0];
 
     print($layout_content);
 }
